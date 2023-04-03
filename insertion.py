e82485938b3b01cf8f3869e2a53e3b8d57a355f7
MagnusAustin <111227859+MagnusAustin@users.noreply.github.com>
2023-04-03 09:42:52 +0530

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    n = len(arr)
+
+    # Traverse through 1 to n
+    for i in range(1, n):
+        key = arr[i]
+
+        # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
